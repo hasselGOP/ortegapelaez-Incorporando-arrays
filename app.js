ceab3ceaf8119b07254b628e1d8abb8ea76d047f
hasselGOP <hassel.ortegapelaez@gmail.com>
2022-08-13 23:22:18 -0500

    agregando objetos y modificando

--- app.js
+++ app.js
@@ -1,66 +1,38 @@
 ///                                              Desafío: Incorporar Arrays
-/* Esta declarando una constante y una variable. */
-const productoN = [];
-let sacar = "";
-alert("Bienvenido a onix glam")
-/**
- * le pide a el usuario añadir un producto y lo agrega a la lista
- */
-function ingresarProducto() {
-    let ingresar;
-    do {
-        ingresar = prompt("Ingresar un nuevo producto");
-    } while (ingresar === "" || ingresar === null);
-    productoN.push(ingresar);
-    let conf = confirm(productoN.join("\n"));
-    if (conf === true) {
-        nuevaListaDeProducto();
-    }
-}
-/*
- * Esta funcion le pregunta al usuario que seleccione entre tres opciones, y dependiendo de la opcion se hara lo que cada case muestre. nuevo producto, visualizar producto o eliminar producto añadido a la lista
- */
-function nuevaListaDeProducto() {
-    let nuevaLista;
-
-    do {
-        nuevaLista = parseInt(
-            prompt(`1)Ingresar nuevo producto
-2)Visualizar todos los productos
-3)Eliminar producto
-                   `)
-        );
-        switch (nuevaLista) {
-            case 1:
-                ingresarProducto();
-                break;
-            case 2:
-                productoN.forEach((el) => confirm(el));
-                nuevaListaDeProducto();
-                break;
-            case 3:
-                sacar = prompt(`Cual deseas eliminar?
-                    ${productoN.join("\n")}`) // metodo join une todos los elementos de una matriz
-                eliminar(sacar);
-                nuevaListaDeProducto();
-                break;  
-            default: 
-                alert("Ingresó una opcion invalida");
+/* La clase productosInventario crea objetos los cuales tiene lad propiedades id,
+producto, precio, y tienda. */
+class productosInventario {
+    constructor(id, producto, precio, tienda) {
+        this.id = id
+        this.producto = producto.toUpperCase()
+        this.precio = precio
+        this.tienda = tienda
         }
-    } 
-    while (isNaN(nuevaLista) || nuevaLista === "" || (nuevaLista != 1 && nuevaLista != 2 && nuevaLista != 3 ));
 }
-/**
- * Toma un elemento como argumento, encuentra el index de ese elemento en el array y si existe lo remueve del array.
- * el - El elemento que será removido del array. 
- */
-const eliminar = (el) => {
-    let i = productoN.indexOf(el);
-    if (i != -1) {
-        productoN.splice(i, 1);
+
+const productoN = []
+function agregarInventario() {
+    productoN.push(new  productosInventario(001, "audifonos",  1500, "Lindavista"))
+    productoN.push(new  productosInventario(002, "computadora", 19800, "Mundo E"))
+    productoN.push(new  productosInventario(003, "mouse", 550, "Lindavista"))
+    productoN.push(new  productosInventario(004, "camara", 1850, "Parque delta"))
+    productoN.push(new  productosInventario(005, "Sensor", 1800, "Tezontle"))
+    productoN.push(new  productosInventario(006, "dron", 2400, "Oasis Coyoacan"))
+    productoN.push(new  productosInventario(007, "teclado", 1200, "Perisur"))
+    } 
+
+    agregarInventario();
+    function recorrerInventario() {
+        productoN.forEach(element => {
+            console.table(element)
+        })
     }
-};
-/* LLamando a las funciones. */
-ingresarProducto();
-nuevaListaDeProducto();
-eliminar();
+alert("Onix glam cdmx la tienda Online mas grande de Mexico")
+
+/* Le pregunta al usuario el nombre del producto y luego gracias a filter lo que hara sera filtrar en el array de productos hasta encontrar el producto que coincida con lo que el usuario escribio */
+pedido = prompt("Que producto te interesa comprar?  \n Los siguientes son los productos con los que contamos \naudifonos \ncomputadora \nmouse \ncamara \nsensor \ndron \nteclado").toUpperCase()
+let buscar = productoN.filter(el => el.producto.includes(pedido))   
+
+alert("Perfecto, a continuación te diremos cual es el Producto de tu eleccion, Precio y en que tienda fisica puedes encontrar nuestro producto")    
+alert(`El producto elegido es: \n ${buscar[0].id}.-${buscar[0].producto} con un precio de $${buscar[0].precio} MXN y contamos con este producto en la tienda fisca de ${buscar[0].tienda}`)
+alert(`Gracias por elegirnos, vuelve pronto`)
